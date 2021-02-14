2917460e66160877e10f78f1e52a5df6773dca65
AlexeyMonarh <monarhweb@gmail.com>
2021-02-14 04:22:01 +0300

    Проектная-14-1

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const cardRoutes = require('./cards');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
+// router.use('/sign-up', );
+// router.use('/sign-in', );
 router.use('*', (req, res) => {
   res.status(500).send({ message: 'На сервере произошла ошибка!' });
 });
